3d66cd24ce8f128548ffd2d9e4cf3324890a39fe
cgm-style <84002015+cgm-style@users.noreply.github.com>
2021-05-31 19:03:29 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,37 @@
 const weather = document.querySelector(".js-weather"),
       timeChk = document.querySelector(".js-timeChk"),
-      timeChkOption = timeChk.querySelector("option"),
-      js_NowWeather = document.querySelector(".js_NowWeather");
-let weatherChk = 0;
-let chk_Number = weather.childNodes.length + 5;
-let chkTime =  null;
+      timeChkOption = timeChk.querySelector(".js-timeChk option"),
+      js_NowWeather = document.querySelector(".js_NowWeather"),
+      prevBtn = document.querySelector("#prevBtn"),
+      nextBtn = document.querySelector("#nextBtn"),
+      API_KEY = 'c65eec58bb72eba4ba267415b3d37432',
+      COORDS = 'coords',
+      selectMonth = document.querySelector("#jsMonth form select"),
+      monthText = document.querySelectorAll("#jsMonth form select option"),
+      calendarBtn = document.querySelector("#calendarBtn"),
+      tableBox = document.querySelector("#tableBox"),
+      nowDate = new Date(),
+      Tbody = document.querySelector("#jsTbody"),
+      calendarContainerBox = document.querySelector("#calendarBox"),
+      jsCalenderTitle = document.querySelector("#jsCalenderTitle");
 
-const API_KEY = 'c65eec58bb72eba4ba267415b3d37432';
-const COORDS = 'coords';
+
+let weatherChk = 0,
+    chk_Number = weather.childNodes.length + 5,
+    chkTime =  null,
+    calendarToggle = false,
+    nowDays = nowDate.getDate(),
+    nowMonth = nowDate.getMonth(),
+    nowyers = nowDate.getFullYear(),
+    days = false,
+    countDays = 1,
+    nextDate = 1,
+    date = new Date(nowyers,nowMonth,nowDays),
+    firstDate = new Date(nowyers,nowMonth,1),
+    lastDate = new Date(nowyers,nowMonth+1,0),
+    prevMonthFirstDate = new Date(nowyers,nowMonth-1,1),
+    prevMonthLastDate = new Date(nowyers,nowMonth,0);
+
 
 
 function getWeather(lat, lng)   {
@@ -123,7 +147,165 @@ function changeTime(event)   {
     loadCoords();
 }
 
+
+function addCalendar()  {
+    let checkd = 0,
+    firstDate = new Date(nowyers,nowMonth,1),
+    countDays = 1,
+    nextDate = 1;
+    
+    for(let adtr=1; adtr<=6; adtr++ )  {    // 달력의 몸통부분을 만드는 반복문 
+        const addtr = document.createElement("tr");
+        addtr.id = `tr${adtr}`;
+        Tbody.appendChild(addtr);
+        for(let adtd=1; adtd<=7; adtd++)   {
+            const   addtd = document.createElement("td"),
+                    dayChack = firstDate.getDay()+1;
+
+            let checkLenght = Tbody.rows.length;
+
+                checkd = checkd+1;
+
+            addtr.appendChild(addtd);
+            addtd.className = `td${checkd}`;
+            addtd.name = checkd;
+
+            const nameChack = addtd.name;
+
+            checktd(dayChack,nowMonth);
+
+            const TbodyTd = addtd;
+
+            TbodyTd.addEventListener("mousedown",handleTdCalender);
+        }
+    }
+}
+
+function checktd(firstDay,nowMonth)  {
+    const tdList = document.querySelectorAll("#jsTbody tr:last-child td:last-child");
+
+    firstDate = new Date(nowyers,nowMonth,1)
+    lastDate = new Date(nowyers,nowMonth+1,0)
+    prevMonthFirstDate = new Date(nowyers,nowMonth-1,1)
+    prevMonthLastDate = new Date(nowyers,nowMonth,0)
+
+    
+
+    monthText[nowMonth].defaultSelected = true;
+
+
+    if(days === false){
+        if(tdList[0].name === firstDay) {
+            tdList[0].innerText = `${countDays}`;
+            tdList[0].style.color = "white";
+            days = true;
+        } else  {
+            let prevDate = prevMonthLastDate.getDate()+1;
+            prevDate = prevDate - (firstDay - tdList[0].name);
+            tdList[0].innerText = `${prevDate}`;
+            tdList[0].style.color = "gray";
+        }
+    } else {
+        if (countDays < lastDate.getDate())  {
+            countDays = countDays + 1;
+            tdList[0].innerText = `${countDays}`;
+            tdList[0].style.color = "white";
+            return false;
+        }else  {
+            tdList[0].innerText = `${nextDate}`;
+            nextDate = nextDate + 1;
+            tdList[0].style.color = "gray";
+        }
+    }
+}
+
+function prevMonthCalendar()    {
+    if(nowMonth >= 1)   {
+        nowMonth = nowMonth - 1;
+        date = new Date(nowyers,nowMonth,nowDays);
+
+        Tbody.innerHTML = "";
+        days = false,
+        countDays = 1,
+        nextDate = 1;
+
+        addCalendar();
+    }
+}
+
+function nextMonthCalendar()    {
+    if(nowMonth < 11)   {
+        nowMonth = nowMonth + 1;
+        date = new Date(nowyers,nowMonth,nowDays);
+
+        Tbody.innerHTML = "";
+        days = false,
+        countDays = 1,
+        nextDate = 1;
+
+        addCalendar();
+    }
+}
+
+function selectMonthCalendar()  {
+    let selectMonthValue = selectMonth.selectedIndex+1;
+    nowMonth = selectMonthValue;
+
+    date = new Date(nowyers,nowMonth,nowDays);
+
+    Tbody.innerHTML = "";
+    days = false,
+    countDays = 1,
+    nextDate = 1;
+
+    addCalendar();
+}
+
+function handlecalendarBtn()    {
+    if(calendarToggle === false)    {
+        calendarBtn.className = "calendarOn";
+        tableBox.className = "tableBoxOn"
+        calendarToggle = true;
+
+    } else  {
+        calendarBtn.className = "";
+        tableBox.className = ""
+        calendarToggle = false;
+    }
+}
+
+function handleTdCalender(event) {
+    calendarContainerBox.className = "";
+    toDoList.innerHTML = "";
+    toDoEndList.innerHTML = "";
+
+    const checkTd = event.target;
+    
+    let checkMonth = selectMonth.selectedIndex+1;
+
+    console.log(checkTd,checkMonth);
+
+    TODOS_LS = `${nowyers}-${checkMonth}-${checkTd.innerText}`;
+    
+    loadToDos();
+
+    setTimeout(() => {
+        calendarContainerBox.className = "calenderBoxOn";
+    }, 300);
+
+    console.dir(jsCalenderTitle);
+    console.log(`${nowyers}-${checkMonth}-${checkTd.innerText}`);
+
+    jsCalenderTitle.innerText = `${nowyers}-${checkMonth}-${checkTd.innerText}`;
+}
+
+selectMonth.addEventListener("change",selectMonthCalendar);
+prevBtn.addEventListener("click",prevMonthCalendar);
+nextBtn.addEventListener("click",nextMonthCalendar);
+calendarBtn.addEventListener("click",handlecalendarBtn);
+
 function init() {
     loadCoords();
+    addCalendar();
 }
 init();
